8da6f271cb5802c4363048fc866864750a5edc26
unknown <miraclendukwe51@gmail.com>
2022-08-03 14:29:22 +0100

    installed axios and added telegram bot app js

---
+++ My_Telegram_Bot_App.js
@@ -0,0 +1,69 @@
+const  express = require("express")
+const axios = require("axios")
+
+const app = express()
+//5413442689:AAF72aDW97J06Bi5WweLbpZInQk7KVcHLfY
+
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+
+app.post("/gamabot", (req, res) => {
+    let {message} = req.body
+    let fullresponse = ""
+
+    if (!message){
+        axios.post("https://api.telegram.org/bot5413442689:AAF72aDW97J06Bi5WweLbpZInQk7KVcHLfY/sendMessage", {
+            chat_id: message.chat.id,
+            text: `My current functions now, includes: \nGetting Word Definitions - Just type wordhelp <word>`
+        })
+        res.end()
+    }
+
+    if (message.text.toLowerCase().indexOf("wordhelp") !== -1){
+        let m = message.text.toLowerCase()
+        m.replace("wordhelp", "")
+        m.replaceAll(" ", "")
+
+        const options = {
+            method: 'GET',
+            url: `https://wordsapiv1.p.rapidapi.com/words/${m}`,
+            headers: {
+              'X-RapidAPI-Key': '1145743444msh1f62d6aff7eb38fp12fad3jsn9bb76a81f007',
+              'X-RapidAPI-Host': 'wordsapiv1.p.rapidapi.com'
+            }
+        };
+          
+        axios.request(options).then(function (response) {
+            fullresponse = response
+        }).catch(function (error) {
+            console.error(error);
+        });
+
+        let reply = ``
+        
+        fullresponse.results.map((result) => {
+            reply += `Definition: ${result.definition}\n`
+            reply += `Part of Speech: ${result.partOfSpeech}\n`
+            reply += `Syllables: ${result.synonyms.map((i) => {return i})}\n`
+            reply += `Derivation: ${result.derivation.map((i) => {return i})}\n`
+            reply += `Examples: ${result.examples.map((i) => {return i})}\n`
+        })
+
+        reply += `${results.pronunciation.all}\n`
+        reply += "Next : \n"
+
+        axios.post("https://api.telegram.org/bot5413442689:AAF72aDW97J06Bi5WweLbpZInQk7KVcHLfY/sendMessage", {
+            chat_id: message.chat.id,
+            text: reply
+        }).then((response) => {
+            res.end()
+        }).catch((err) => {
+            console.error(err)
+            res.send("Error: " + err)
+        })
+    }
+})
+
+app.listen(5000, () => {
+    console.log("Listening on port 5000")
+})
