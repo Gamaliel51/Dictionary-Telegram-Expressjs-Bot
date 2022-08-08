4d00ee9c7efae8812e6b0fc5c2e5bbb2da394b01
unknown <miraclendukwe51@gmail.com>
2022-08-08 19:48:13 +0100

    started flutterwave transfer app and stopped unitl react front is running on heroku

--- Cash_Transfer_Practice.js
+++ Cash_Transfer_Practice.js
@@ -3,10 +3,17 @@ const mongoose = require("mongoose")
 const passport = require("passport")
 const cashUsers = require("./cashdbLogin")
 const sessions = require("express-session")
+const Flutterwave = require("flutterwave-node-v3")
+const {getFee, cardCharge} = require("./flutterwaveFunctions")
+const path = require("path")
+const dot = require("dotenv")
+dot.config()
+
 
 app = express()
 timeout = 1000*60*5
 
+app.use(express.static("./public"))
 app.use(sessions({
     secret: 'Thisisarandomkey',
     saveUninitialized: true,
@@ -25,8 +32,26 @@ passport.use(cashUsers.createStrategy())
 passport.serializeUser(cashUsers.serializeUser())
 passport.deserializeUser(cashUsers.deserializeUser())
 
+app.get('/', (req, res) => {
+    way = path.join(__dirname, "public", "paytest.html")
+    res.sendFile(way)
+});
+
+app.get("/checkfee", (req, res) => {
+    getFee(req, res)
+    
+})
+
 app.post("/", passport.authenticate('local', {failureRedirect: "/"}), (req, res) => {
     res.send("hello")
+    res.end()
+})
+
+app.post("/transfer",  (req, res) => {
+    console.log(req.body)
+    let {fullname, card_number, expirationdate, cvv, email, amount} = req.body
+    let redirect = "http://localhost:5000"
+    cardCharge(req, res, card_number, cvv, expirationdate.slice(0,2), expirationdate.slice(2), email, fullname, redirect, String(amount))
     res.end()
 })
 
